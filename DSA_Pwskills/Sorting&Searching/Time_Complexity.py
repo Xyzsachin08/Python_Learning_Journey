9a2d963c6ec6fa291eef9cb4272f352f44f628da
Sachin Borude <sachinborude0610@gmail.com>
2026-03-22 22:30:07 +0530

    Modify File Structure

---
+++ DSA_Pwskills/Sorting&Searching/Time_Complexity.py
@@ -0,0 +1,35 @@
+''' = int(input("Enter a number if you want print hello"))
+for i in range(n):
+    for j in range(n):
+        for s in range(n):
+            print("I love you sachin")'''
+            
+'''def my_print(n):
+    i = 10
+    while i < n:
+        print("hello")
+        i = i * 2'''
+        
+        
+'''x = 9
+
+if x == 5:
+    print("x is 5")
+elif x == 10:
+    print("x is 10")
+elif x == 8:
+    print("x is 8")
+elif x == 9:
+    print("x is 9")
+else:
+    print("x is something else")'''
+    
+    
+'''x = 1
+while x < 10:
+    print("Hello")
+    x = x + 1'''
+    
+    
+    
+
