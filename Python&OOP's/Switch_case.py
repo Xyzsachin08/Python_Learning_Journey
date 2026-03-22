9f2c25086be1d6db1b92cf9559d7bf6b0429cf66
Sachin Borude <sachinborude0610@gmail.com>
2026-03-22 22:36:54 +0530

    Modify File Structure

---
+++ Python&OOP's/Switch_case.py
@@ -0,0 +1,19 @@
+Day_num = int(input("Enter a Correct number:-"))
+
+match Day_num:
+    case 1:
+        print("Monday")
+    case 2:
+        print("Tuesday")
+    case 3:
+        print("Wednesday")
+    case 4:
+        print("Thursday")
+    case 5:
+        print("Friday")
+    case 6:
+        print("Saturday")
+    case 7:
+        print("Sunday")
+    case _:  
+        print("xyz")
