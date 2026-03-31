9fac2382a5fd3c9ee55abde2833259ef4a719a30
Sachin Borude <sachinborude0610@gmail.com>
2026-03-31 11:06:10 +0530

    While loop

---
+++ Advance Python Learning journey/Concept_02/while_loop.py
@@ -0,0 +1,58 @@
+'''1. Print numbers from 1 to 10
+
+i = 1
+while i<=10:
+    print(i)
+    i+=1
+ 
+ 
+ 
+ 2. Print even numbers from 1 to 20
+
+n = 2
+while n<=20:
+        print(n)
+        n+=2
+  
+  
+3. Print numbers from 10 to 1 (reverse)
+
+n = 10
+while(n >=1):
+    print(n)
+    n -=1
+
+
+
+4. Sum of first n numbers
+
+n = int(input("Enter a number: "))
+
+i = 1
+sum = 0
+while(i<=n):
+    sum = sum+i
+    i +=1
+    
+print("sum is", sum)
+
+
+
+5. Number guessing game (basic)
+
+'''
+secret = 7
+while True:
+    guess = int(input("Enter a number: "))
+    if guess == secret:
+        print("Correct!")
+    else:
+        print("Try another")
+
+
+
+
+
+ 
+
+    
