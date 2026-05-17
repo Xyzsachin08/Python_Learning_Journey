a0b23b3ae9f362d85a5f2ea91999faa8b1cd2c4d
Sachin Borude <sachinborude0610@gmail.com>
2026-05-17 08:00:30 +0530

    encapsulation (protected)

---
+++ Advance Python Learning journey/Concept_05/Encapsulation/03_Protected.py
@@ -0,0 +1,8 @@
+class Employee:
+    def __init__(self):
+        self._salary = 5000
+e = Employee()
+print(e._salary)
+
+
+# in protected case we can use protected outside the class but we languge tell you shoulde not use outside the class reason data safty .
