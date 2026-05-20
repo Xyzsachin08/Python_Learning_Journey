688f85895fbaccc5c06c6af46b884c8191f9dc2f
Sachin Borude <sachinborude0610@gmail.com>
2026-05-20 09:54:00 +0530

    single inheritance

---
+++ Advance Python Learning journey/Concept_05/Inheritance/Single.py
@@ -0,0 +1,12 @@
+class Swara:
+    def swara_bike(self):
+        print("Swara bike color is red")
+        
+class Shruti(Swara):
+    def shruti_bike(self):
+        print("Shruti bike color is yellow")
+        
+s = Shruti()
+s.swara_bike()
+s.shruti_bike()
+        
